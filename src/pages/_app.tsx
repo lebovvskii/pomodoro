aa196b09295de2236f945e6dcfe39c4649d175b0
Nikita Lebovvskii <lebovvskii.design@ya.ru>
2024-02-13 13:34:48 +1000

    starter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
-
-export function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
